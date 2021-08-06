c1f970fe1d80f93d61953a9ff84662db53dc60d9
Sulaiman <sulaimanmk24@gmail.com>
2021-08-06 16:02:15 +0100

    Completed Testimonials Section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,5 @@
-    import '../styles/styles.css'
+import '../styles/styles.css'
 
-    if (module.hot) {
-      module.hot.accept()
-    }
-  //  alert("Hello, Testing...Testing...its a Madnezzz!!!!")//
+if (module.hot) {
+  module.hot.accept()
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,34 @@
-const path = require("path")
+const path = require('path')
 
 const postCSSPlugins = [
   require('postcss-import'),
   require('postcss-mixins'),
-  require("postcss-simple-vars"), 
-  require("postcss-nested"), 
-  require("autoprefixer")
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('autoprefixer')
 ]
 
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: './app/assets/scripts/App.js',
   output: {
-    filename: "bundled.js",
-    path: path.resolve(__dirname, "app")
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
   },
   devServer: {
     before: function(app, server) {
       server._watch('./app/**/*.html')
     },
-    contentBase: path.resolve(__dirname, "app"),
+    contentBase: path.join(__dirname, 'app'),
     hot: true,
     port: 3000,
     host: '0.0.0.0'
   },
-
-  mode: "development",
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+        use: ["style-loader", "css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
       }
     ]
   }
